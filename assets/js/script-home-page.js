1d68a029309bc3d58771ac35b302b29bdbc8648b
shantoe <shantoroy1792@gmail.com>
2022-03-08 22:20:56 +0600

    Second Update

--- assets/js/script-home-page.js
+++ assets/js/script-home-page.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
-	// CARD SLIDE
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+     // video Popup start 
+	 $('.Video_popup').magnificPopup({
+	  type: 'iframe',
+	    iframe: {
+	      markup: '<div class="mfp-iframe-scaler">'+
+	            '<div class="mfp-close"></div>'+
+	            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+	          '</div>', 
 
+	      patterns: {
+	        youtube: {
+	        index: 'youtube.com/', 
+
+	        id: 'v=',
+	      
+	        src: 'https://www.youtube.com/embed/%id%?autoplay=1'  
+	        }
+	      
+	      },
+	      
+	      srcAction: 'iframe_src', 
+		}
+	});
 });

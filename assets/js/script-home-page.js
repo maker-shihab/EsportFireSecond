14b55639c8ecd6e16c1de586bcf2206f5aece449
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-26 00:43:06 +0600

    some Update

--- assets/js/script-home-page.js
+++ assets/js/script-home-page.js
@@ -1,37 +1,36 @@
 $(document).ready(function() {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-     // video Popup start 
-	 $('.Video_popup').magnificPopup({
-	  type: 'iframe',
-	    iframe: {
-	      markup: '<div class="mfp-iframe-scaler">'+
-	            '<div class="mfp-close"></div>'+
-	            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-	          '</div>', 
-
-	      patterns: {
-	        youtube: {
-	        index: 'youtube.com/', 
-
-	        id: 'v=',
-	      
-	        src: 'https://www.youtube.com/embed/%id%?autoplay=1'  
-	        }
-	      
-	      },
-	      
-	      srcAction: 'iframe_src', 
-		}
-	});
+	   // video Popup start 
+		 $('.Video_popup').magnificPopup({
+			type: 'iframe',
+				iframe: {
+					markup: '<div class="mfp-iframe-scaler">'+
+								'<div class="mfp-close"></div>'+
+								'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+							'</div>', 
+	
+					patterns: {
+						youtube: {
+						index: 'youtube.com/', 
+	
+						id: 'v=',
+					
+						src: 'https://www.youtube.com/embed/%id%?autoplay=1'  
+						}
+					
+					},
+					
+					srcAction: 'iframe_src', 
+			}
+		});
+    // $('.counter').counterUp({
+    //     delay: 10,
+    //     time: 1000
+    // });
 });
 $('.games-slider').owlCarousel({
-	loop:true,
-	margin:30,
+	// loop:true,
 	dots: false,
-	autoplay: true,
+	// autoplay: true,
 	nav:true,
 	responsive:{
 		0:{
